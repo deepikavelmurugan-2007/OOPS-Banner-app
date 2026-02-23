6d5cf41836937ec6f251d5065b33f4518f8c004e
deepikavelmurugan-2007 <deepikavelmurgan2007@gmail.com>
2026-02-23 08:48:59 +0530

    UC3-string

---
+++ UC3OOPS.java
@@ -0,0 +1,15 @@
+
+/** UC3 - Printing OOPS using Sting.join()*/
+public class UC3OOPS {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("  ", " ***** ", " ***** ", " ***** ",  "****** "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*     *",  "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*     *",  "*      "));
+        System.out.println(String.join("  ", "*     *", "*     *", " ***** ",  "*****  "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*       ", "    * "));
+        System.out.println(String.join("  ", "*     *", "*     *", "*       ", "    * "));
+        System.out.println(String.join("  ", " ***** ", " ***** ", "*       ", "*****"));
+
+    }
+}
