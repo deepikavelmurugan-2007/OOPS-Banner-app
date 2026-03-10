ee56994da1b4a0c0576f795aedfeffe3fc976eb1
deepikavelmurugan-2007 <deepikavelmurgan2007@gmail.com>
2026-03-10 10:40:39 +0530

    Add files via upload

---
+++ KM.java
@@ -0,0 +1,12 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+
+        double kilometers = 10.8;
+        double miles;
+
+        // Given: 1 km = 1.6 miles
+        miles = kilometers * 1.6;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
