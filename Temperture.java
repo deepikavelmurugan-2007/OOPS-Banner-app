b130fa394b71bc57c5f7fe6a8620e88709cde7a2
deepikavelmurugan-2007 <deepikavelmurgan2007@gmail.com>
2026-03-10 10:45:14 +0530

    Add files via upload

---
+++ Temperture.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TemperatureConversion {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double fahrenheit;
+        double celsiusResult;
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        fahrenheit = sc.nextDouble();
+
+        celsiusResult = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("The " + fahrenheit + " fahrenheit is " 
+                           + celsiusResult + " celsius");
+
+        sc.close();
+    }
+}
