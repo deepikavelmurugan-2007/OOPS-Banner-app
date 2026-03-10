85f882c3555f0ecd6a36602831b940eb37ffcfc4
deepikavelmurugan-2007 <deepikavelmurgan2007@gmail.com>
2026-03-10 10:41:58 +0530

    Add files via upload

---
+++ Earth.java
@@ -0,0 +1,18 @@
+public class EarthVolume {
+    public static void main(String[] args) {
+
+        double radius = 6378; 
+        double pi = 3.14159;
+
+        double volumeKm;
+        double volumeMiles;
+
+        volumeKm = (4.0/3.0) * pi * radius * radius * radius;
+
+        volumeMiles = volumeKm * 0.239913;
+
+        System.out.println("The volume of earth in cubic kilometers is " 
+                            + volumeKm + 
+                           " and cubic miles is " + volumeMiles);
+    }
+}
